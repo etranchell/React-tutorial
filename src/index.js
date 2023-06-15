5536a9bf5107141d1fafc5b65a99b66d3e872aea
eric <erictranchell@yahoo.com>
2023-06-15 01:48:32 -0400

    save

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import { data } from './data';
-import SpecificBook from './Book';
+import SpecificBook from './Books';
 import { greeting } from './testing/testing';
 
 function Booklist() {
